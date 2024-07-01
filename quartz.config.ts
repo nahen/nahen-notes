7c6f828b03252c264229824af4327d451dc9c37d
nahen <resalneerg@gmail.com>
2024-07-01 19:37:45 +0900

    Quartz sync: Jul 1, 2024, 7:37 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "nahen.github.io/nahen-notes",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
