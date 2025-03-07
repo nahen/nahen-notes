629d3ae8f43258c7e3688e2f127d3f4e7b26861e
nahen <resalneerg@gmail.com>
2025-03-07 10:34:19 +0900

    chore(config): ヘッダ・ボディのフォントを指定しないとビルドできないようなので明示

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,8 +23,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "",
-        body: "",
+        header: "IBM Plex Sans JP",
+        body: "IBM Plex Sans JP",
         code: "IBM Plex Mono",
       },
       colors: {
