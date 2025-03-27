90efc9fef6b1515abb5a826c5da3203d051f7d0f
nahen <resalneerg@gmail.com>
2025-03-27 10:57:20 +0900

    refactor(ContentMeta): 外部リンク作成機能を外部関数として抽出

--- quartz/components/ContentMeta.tsx
+++ quartz/components/ContentMeta.tsx
@@ -1,8 +1,8 @@
-import { Date, getDate } from "./Date"
+import { Date } from "./Date"
 import { QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import readingTime from "reading-time"
 import { classNames } from "../util/lang"
-import { i18n } from "../i18n"
+import { i18n, ValidLocale } from "../i18n"
 import { JSX } from "preact"
 import style from "./styles/contentMeta.scss"
 
@@ -17,6 +17,29 @@ interface ContentMetaOptions {
 const defaultOptions: ContentMetaOptions = {
   showReadingTime: false,
   showComma: true,
+}
+
+function getFileExtension(url: string): string {
+  const fileName = url.split('/').pop();
+  if (fileName && fileName.includes('.')) {
+    return fileName.split('.').pop() || '';
+  }
+  return '';
+}
+
+function createExternalLink(url: string, locale: ValidLocale): JSX.Element {
+  const text = getFileExtension(url) === "pdf"
+    ? i18n(locale).components.contentMeta.linkPdf
+    : i18n(locale).components.contentMeta.linkText;
+
+  return (
+    <a class="external" target="_blank" href={url}>
+      {text}
+      <svg aria-hidden="true" class="external-icon" viewBox="0 0 512 512">
+        <path d="M320 0H288V64h32 82.7L201.4 265.4 178.7 288 224 333.3l22.6-22.6L448 109.3V192v32h64V192 32 0H480 320zM32 32H0V64 480v32H32 456h32V480 352 320H424v32 96H64V96h96 32V32H160 32z"></path>
+      </svg>
+    </a>
+  );
 }
 
 export default ((opts?: Partial<ContentMetaOptions>) => {
@@ -47,29 +70,8 @@ export default ((opts?: Partial<ContentMetaOptions>) => {
       }
 
       // YAMLにurlがあれば表示する
-      if (fileData.frontmatter && fileData.frontmatter["url"]) {
-          const url = fileData.frontmatter["url"] as string
-
-          let text: string = ""
-          if (getFileExtension(url) == "pdf") {
-            text = i18n(cfg.locale).components.contentMeta.linkPdf
-          } else {
-            // 空文字列の場合はWebサイト
-            text = i18n(cfg.locale).components.contentMeta.linkText
-          }
-          segments.push(<a class="external" target="_blank" href={url}>{text}<svg aria-hidden="true" class="external-icon" viewBox="0 0 512 512"><path d="M320 0H288V64h32 82.7L201.4 265.4 178.7 288 224 333.3l22.6-22.6L448 109.3V192v32h64V192 32 0H480 320zM32 32H0V64 480v32H32 456h32V480 352 320H424v32 96H64V96h96 32V32H160 32z"></path></svg></a>)
-      }
-
-      function getFileExtension(url: string): string {
-        // URLの最後のセグメントを取得
-        const fileName = url.split('/').pop();
-        // ファイル名が存在し、ドットを含む場合
-        if (fileName && fileName.includes('.')) {
-          // 最後のドット以降の文字列（拡張子）を返す
-          return fileName.split('.').pop() || '';
-        }
-        // 拡張子が見つからない場合は空文字列を返す
-        return '';
+      if (fileData.frontmatter?.url) {
+        segments.push(createExternalLink(fileData.frontmatter.url as string, cfg.locale))
       }
 
       return (
